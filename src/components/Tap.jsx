cc14707551b9a61904072d29c40b4b8570c7c1fd
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-11-16 11:23:55 -0800

    Add menu and tap components. Add looping. Working on displaying images

--- src/components/Tap.jsx
+++ src/components/Tap.jsx
@@ -3,32 +3,36 @@ import PropTypes from 'prop-types';
 
 function Tap(props){
   const drinkStyle = {
-    width: '200px',
+    width: '500px',
     background: 'silver',
+    height: '400px',
+  }
+  const imgStyle = {
+    height: '400px',
   }
   return (
-    <div>
-        <style jsx>{`
-          div {
-            background-color: gray;
-          }
-        `}</style>
-      <div className="row">
+    <div style={drinkStyle}>
       <h2>{props.name}</h2>
       <p>{props.brewer}</p>
       <p><em>{props.description}</em></p>
       <p>{props.abv}</p>
       <p>{props.price}&</p>
       <p>{props.remaining}</p>
+      <p style={imgStyle}>{props.image}</p>
       <hr/>
     </div>
+    
   );
 }
 
 Tap.propTypes = {
-  names: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  names: PropTypes.string,
+  brewer: PropTypes.string,
+  description: PropTypes.string,
+  abv: PropTypes.string,
+  price: PropTypes.string,
+  remaining: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Tap;
