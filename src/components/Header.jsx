6f9ad8b357f39492016762631d81dbbf1e6bd47f
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-11-30 08:25:24 -0800

    add eslint rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-  let imgUrl='https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hnm5qgf5ur9fsiemsefum58ea3-20171019173603.Medi.jpeg'
+  let imgUrl =
+    "https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hnm5qgf5ur9fsiemsefum58ea3-20171019173603.Medi.jpeg";
   const style = {
-    backgroundColor: 'black',
-    color: 'white',
-    fontFamily: 'Allerta Stencil',
-    padding: '20px',
-    textAlign: 'center',
-    left: '0',
-    width: '100%',
-    fontSize: '36px',
-    marginTop: '1px',
-    backgroundImage: 'url("https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hnm5qgf5ur9fsiemsefum58ea3-20171019173603.Medi.jpeg")', 
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-
-  }
-  return(
+    backgroundColor: "black",
+    color: "white",
+    fontFamily: "Allerta Stencil",
+    padding: "20px",
+    textAlign: "center",
+    left: "0",
+    width: "100%",
+    fontSize: "36px",
+    marginTop: "1px",
+    backgroundImage:
+      'url("https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hnm5qgf5ur9fsiemsefum58ea3-20171019173603.Medi.jpeg")',
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat"
+  };
+  return (
     <div style={style}>
       <h1>Welcome to AJ's Tap Room!!!</h1>
+      <i className="fa fa-heart" aria-hidden="false" />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const style = {
-    backgroundColor: 'gray',
-    color: '#fff2e6',
-    fontFamily: 'Allerta Stencil',
-    padding: '20px',
-    textAlign: 'right',
-    width: '99%',
-    fontSize: '10px',
-    marginTop: '1px',
-    height: '20px',
-  }
-  const textStyle={
-    marginRight: '20px',
-  }
-  return(
+    backgroundColor: "gray",
+    color: "#fff2e6",
+    fontFamily: "Allerta Stencil",
+    padding: "20px",
+    textAlign: "right",
+    width: "99%",
+    fontSize: "10px",
+    marginTop: "1px",
+    height: "20px"
+  };
+  const textStyle = {
+    marginRight: "20px"
+  };
+  return (
     <div>
-      <div className="Navbar" style={style} >
-      <h3 style={textStyle}>About | <Link to="/Admin">Admin</Link></h3>
+      <div className="Navbar" style={style}>
+        <h3 style={textStyle}>
+          About | <Link to="/Admin">Admin</Link>
+        </h3>
       </div>
     </div>
   );
